50c4307b07d7b77ac967a8fcfe24b238441e77b0
Aryuna Rabdanova <rabdanova.aryuna@mail.ru>
2024-01-18 19:13:04 +0700

    Saving only if user is valid

--- src/post_registrate.php
+++ src/post_registrate.php
@@ -1,16 +1,21 @@
 <?php
 
+$mainFlag = true;
+
 $name = $_POST['name'];
 if (empty($name)) {
     echo 'Имя должно быть заполнено';
+    $mainFlag = false;
 }
 if (strlen($name) < 2) {
     echo 'Имя должно быть двух символов';
+    $mainFlag = false;
 }
 
 $email = $_POST['email'];
 if (empty($email)) {
     echo 'Почта должна быть заполнена';
+    $mainFlag = false;
 }
 $flag = false;
 for ($i = 0; $i < strlen($email); $i++) {
@@ -20,23 +25,28 @@ for ($i = 0; $i < strlen($email); $i++) {
 }
 if ($flag === false) {
     echo 'Почта должна содержать "@"';
+    $mainFlag = false;
 }
 
 $password = $_POST['psw'];
 if (empty($password)) {
     echo 'Пароль должен быть заполнен';
+    $mainFlag = false;
 }
 $passwordRep = $_POST['psw-repeat'];
 if (empty($passwordRep)) {
     echo 'Повторите пароль';
+    $mainFlag = false;
 }
 if ($password !== $passwordRep) {
     echo 'Пароли должны совпадать';
+    $mainFlag = false;
 }
 
 $pdo = new PDO("pgsql:host=db; port=5432; dbname=db", "aryuna", "030201");
 
-$stmt = $pdo->prepare("INSERT INTO users (name, email, password) VALUES (:name, :email, :password)");
-$stmt->execute(['name' => $name, 'email' => $email, 'password' => $password]);
-
+if ($mainFlag === true) {
+    $stmt = $pdo->prepare("INSERT INTO users (name, email, password) VALUES (:name, :email, :password)");
+    $stmt->execute(['name' => $name, 'email' => $email, 'password' => $password]);
+}
 //$pdo->exec("INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')");
