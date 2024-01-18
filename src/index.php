06217759c077f8999d1867fa3b215eab822fc6d5
Aryuna Rabdanova <rabdanova.aryuna@mail.ru>
2024-01-18 19:00:25 +0700

    Validation done

--- src/index.php
+++ src/index.php
@@ -2,7 +2,7 @@
 
 $pdo = new PDO("pgsql:host=db; port=5432; dbname=db", "aryuna", "030201");
 
-$pdo->exec('CREATE TABLE test1 (id serial not null, name varchar (255) not null)');
+$pdo->exec('CREATE TABLE test (id serial not null, name varchar (255) not null)');
 
 //$pdo->exec("INSERT INTO test1 (name) VALUES ('Ivan')");
 //
--- src/post_registrate.php
+++ src/post_registrate.php
@@ -1,11 +1,42 @@
 <?php
 
 $name = $_POST['name'];
-$email = $_POST['email'];
-$password = $_POST['psw'];
-$passwordRep = $_POST['psw-repeat'];
+if (empty($name)) {
+    echo 'Имя должно быть заполнено';
+}
+if (strlen($name) < 2) {
+    echo 'Имя должно быть двух символов';
+}
 
+$email = $_POST['email'];
+if (empty($email)) {
+    echo 'Почта должна быть заполнена';
+}
+$flag = false;
+for ($i = 0; $i < strlen($email); $i++) {
+    if ($email[$i] === '@') {
+        $flag = true;
+    }
+}
+if ($flag === false) {
+    echo 'Почта должна содержать "@"';
+}
+
+$password = $_POST['psw'];
+if (empty($password)) {
+    echo 'Пароль должен быть заполнен';
+}
+$passwordRep = $_POST['psw-repeat'];
+if (empty($passwordRep)) {
+    echo 'Повторите пароль';
+}
+if ($password !== $passwordRep) {
+    echo 'Пароли должны совпадать';
+}
 
 $pdo = new PDO("pgsql:host=db; port=5432; dbname=db", "aryuna", "030201");
 
-$pdo->exec("INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')");
+$stmt = $pdo->prepare("INSERT INTO users (name, email, password) VALUES (:name, :email, :password)");
+$stmt->execute(['name' => $name, 'email' => $email, 'password' => $password]);
+
+//$pdo->exec("INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')");
