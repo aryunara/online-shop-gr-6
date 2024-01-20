d5a9290bc9f64e5c236e87ef68cb6ba29ba208ad
Aryuna Rabdanova <rabdanova.aryuna@mail.ru>
2024-01-21 04:17:07 +0700

    Сделана страница логина с подключением к бд

---
+++ src/post_login.php
@@ -0,0 +1,29 @@
+<?php
+
+if (isset($_POST['username'])) {
+    $username = $_POST['username'];
+}
+if (isset($_POST['password'])) {
+    $password = $_POST['password'];
+}
+
+$pdo = new PDO("pgsql:host=db; port=5432; dbname=db", "aryuna", "030201");
+
+$stmt = $pdo->prepare('SELECT * FROM users WHERE email = :username');
+$stmt->execute(['username' => $username]);
+$userInfo = $stmt->fetch();
+
+if (empty($userInfo)) {
+    echo 'Неверный email';
+} else {
+    if ($password === $userInfo['password']) {
+        echo "Вы залогинились";
+    } else {
+        echo "Неверный пароль";
+    }
+}
+
+
+
+
+
