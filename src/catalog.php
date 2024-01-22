0129a2c047fa3aa6901de5010237ddfb4de68050
Aryuna Rabdanova <rabdanova.aryuna@mail.ru>
2024-01-22 19:50:05 +0700

    Добавлены примеры продуктов в каталог

--- src/catalog.php
+++ src/catalog.php
@@ -1,17 +1,18 @@
 <div class="container">
-    <h3>Catalog</h3>
+    <h3>Product Catalog</h3>
     <div class="card-deck">
         <div class="card text-center">
             <a href="#">
                 <div class="card-header">
                     Hit!
                 </div>
-                <img class="card-img-top" src="https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ&s=043d89cbf03cbdbbe8ed9f9e5e44ce6f" alt="Card image">
+                <img class="card-img-top" src="https://images.unsplash.com/photo-1635436338433-89747d0ca0ef?q=80&w=1886&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Card image"  width="400"
+                     height="341">
                 <div class="card-body">
-                    <p class="card-text text-muted">Category name</p>
-                    <a href="#"><h5 class="card-title">Very long item name</h5></a>
+                    <p class="card-text text-muted">Dairy products</p>
+                    <a href="#"><h5 class="card-title">Organic milk in a glass bottle (150 ml.)</h5></a>
                     <div class="card-footer">
-                        3000$
+                        2$
                     </div>
                 </div>
             </a>
@@ -21,12 +22,13 @@
                 <div class="card-header">
                     Hit!
                 </div>
-                <img class="card-img-top" src="https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ&s=043d89cbf03cbdbbe8ed9f9e5e44ce6f" alt="Card image">
+                <img class="card-img-top" src="https://images.unsplash.com/photo-1677708892002-ae8423bf4ad3?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Card image" width="400"
+                     height="341">
                 <div class="card-body">
-                    <p class="card-text text-muted">Category name</p>
-                    <a href="#"><h5 class="card-title">Very long item name</h5></a>
+                    <p class="card-text text-muted">Coffee</p>
+                    <a href="#"><h5 class="card-title">"1790 Coffee" signature roast - 100% organic (340 g.)</h5></a>
                     <div class="card-footer">
-                        3000$
+                        17,99$
                     </div>
                 </div>
             </a>
