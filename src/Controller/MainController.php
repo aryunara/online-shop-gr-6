764346ba6a57a0aa2116606eaec70725a8dec6dc
Aryuna Rabdanova <rabdanova.aryuna@mail.ru>
2024-01-23 18:04:01 +0700

    Разметка каталога теперь в цикле и соединена с базой данных

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -10,6 +10,12 @@ class MainController
             header('Location: /get_login.php');
         } else {
             echo "Hello, {$_SESSION['user_name']}!";
+
+            $pdo = new PDO("pgsql:host=db; port=5432; dbname=db", "aryuna", "030201");
+
+            $stmt = $pdo->query('SELECT * FROM products');
+            $products = $stmt->fetchAll();
+
             require_once './../View/catalog.php';
         }
     }
