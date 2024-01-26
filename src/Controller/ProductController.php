806d746a0fa836a3b9fa4e9da5aabc894b1ceddb
Aryuna Rabdanova <rabdanova.aryuna@mail.ru>
2024-01-26 11:40:54 +0700

    Добавлена валидация количества товара (нужно исправить повторное подключение к таблице products)

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -8,7 +8,6 @@ class ProductController
         if (!isset($_SESSION['user_id'])) {
             header('Location: /login');
         } else {
-            echo "Hello, {$_SESSION['user_name']}!";
 
             $pdo = new PDO("pgsql:host=db; port=5432; dbname=db", "aryuna", "030201");
 
@@ -26,14 +25,47 @@ class ProductController
             header('Location: /get_login.phtml');
         }
 
+        $errorsQuantity = $this->validateQuantity($_POST);
+
+        if (empty($errorsQuantity)) {
+            try {
+                $pdo = new PDO("pgsql:host=db; port=5432; dbname=db", "aryuna", "030201");
+                $userId = $_SESSION['user_id'];
+                $productId = $_POST['product-id'];
+                $quantity = $_POST['quantity'];
+
+                $stmt = $pdo->prepare("INSERT INTO user_products (user_id, product_id, quantity) VALUES (:userId, :productId, :quantity)");
+                $stmt->execute(['userId' => $userId, 'productId' => $productId, 'quantity' => $quantity]);
+
+                header('Location: /main');
+            } catch(PDOException) {
+                $errorsQuantity['quantity'] = 'Укажите целое число';
+            }
+        }
         $pdo = new PDO("pgsql:host=db; port=5432; dbname=db", "aryuna", "030201");
-        $userId = $_SESSION['user_id'];
-        $productId = $_POST['product-id'];
-        $quantity = $_POST['quantity'];
 
-        $stmt = $pdo->prepare("INSERT INTO user_products (user_id, product_id, quantity) VALUES (:userId, :productId, :quantity)");
-        $stmt->execute(['userId' => $userId, 'productId' => $productId, 'quantity' => $quantity]);
+        $stmt = $pdo->query('SELECT * FROM products');
+        $products = $stmt->fetchAll();
 
-        echo "Продукты добавлены!";
+        require_once './../View/catalog.phtml';
+    }
+
+    private function validateQuantity() : array
+    {
+        $errorsQuantity = [];
+
+        if (isset($_POST['quantity'])) {
+            $quantity = $_POST['quantity'];
+
+            if (empty($quantity)) {
+                $errorsQuantity['quantity'] = 'Укажите количество';
+            }
+            if ($quantity < 1) {
+                $errorsQuantity['quantity'] = 'Количество должно быть больше 0';
+            }
+        } else {
+            $errorsQuantity['quantity'] = 'Поле quantity не указано';
+        }
+        return $errorsQuantity;
     }
 }
