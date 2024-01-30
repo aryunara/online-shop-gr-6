f46210bb25e7a0f4a5382f8388af9d0d78927c14
Aryuna Rabdanova <rabdanova.aryuna@mail.ru>
2024-01-30 11:49:42 +0700

    Добавлен класс App

---
+++ src/App/App.php
@@ -0,0 +1,54 @@
+<?php
+
+use Controller\ProductController;
+use Controller\UserController;
+
+class App
+{
+    public function run()
+    {
+
+        $requestUri = $_SERVER['REQUEST_URI'];
+        $requestMethod = $_SERVER['REQUEST_METHOD'];
+        if ($requestUri === '/login') {
+            if ($requestMethod === 'GET') {
+                $obj = new UserController();
+                $obj->getLogin();
+            } elseif ($requestMethod === 'POST') {
+                $obj = new UserController();
+                $obj->postLogin();
+            } else {
+                echo "Метод $requestMethod не поддерживается для адреса $requestUri";
+            }
+
+        } elseif ($requestUri === '/registrate') {
+            if ($requestMethod === 'GET') {
+                $obj = new UserController();
+                $obj->getRegistrate();
+            } elseif ($requestMethod === 'POST') {
+                $obj = new UserController();
+                $obj->postRegistrate();
+            } else {
+                echo "Метод $requestMethod не поддерживается для адреса $requestUri";
+            }
+
+        } elseif ($requestUri === '/main') {
+            $obj = new ProductController();
+            $obj->getCatalog();
+        }
+
+        elseif ($requestUri === '/add-product') {
+            $obj = new ProductController();
+            $obj->addProduct();
+        }
+
+        elseif ($requestUri === '/cart') {
+            $obj = new ProductController();
+            $obj->getCartProducts();
+        }
+
+        else {
+            require_once './../View/404.html';
+        }
+    }
+}
--- src/public/index.php
+++ src/public/index.php
@@ -1,51 +1,11 @@
 <?php
 
-$requestUri = $_SERVER['REQUEST_URI'];
-$requestMethod = $_SERVER['REQUEST_METHOD'];
 
-require_once "./../App/Autoloader.php";
-use Controller\ProductController as ProductController;
-use Controller\UserController as UserController;
+require_once './../App/Autoloader.php';
+require_once './../App/App.php';
 
 Autoloader::registrate();
 
-if ($requestUri === '/login') {
-    if ($requestMethod === 'GET') {
-        $obj = new UserController();
-        $obj->getLogin();
-    } elseif ($requestMethod === 'POST') {
-        $obj = new UserController();
-        $obj->postLogin();
-    } else {
-        echo "Метод $requestMethod не поддерживается для адреса $requestUri";
-    }
+$app = new App();
+$app->run();
 
-} elseif ($requestUri === '/registrate') {
-    if ($requestMethod === 'GET') {
-        $obj = new UserController();
-        $obj->getRegistrate();
-    } elseif ($requestMethod === 'POST') {
-        $obj = new UserController();
-        $obj->postRegistrate();
-    } else {
-        echo "Метод $requestMethod не поддерживается для адреса $requestUri";
-    }
-
-} elseif ($requestUri === '/main') {
-    $obj = new ProductController();
-    $obj->getCatalog();
-    }
-
-elseif ($requestUri === '/add-product') {
-    $obj = new ProductController();
-    $obj->addProduct();
-    }
-
-elseif ($requestUri === '/cart') {
-    $obj = new ProductController();
-    $obj->getCartProducts();
-}
-
-else {
-    require_once './../View/404.html';
-}
